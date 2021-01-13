f99f33a948c3205968869a0d615aaae04fc74e3b
cetmann <cetmann@damtp.cam.ac.uk>
2021-01-13 15:30:47 +0000

    Implemented iUNets.

--- ovseg/networks/iUNet.py
+++ ovseg/networks/iUNet.py
@@ -1,13 +1,47 @@
 import torch.nn as nn
-
+from iunets import iUNet as iUNetModel
 
 class iUNet(nn.Module):
+    """Implements the invertible U-Net.
 
-    def __init__(self, in_channels: int, out_channels: int, kernel_sizes: list):
+    The keyword arguments are the same as with the iUNet library.
+    (github.com/cetmann/iunets)
+    """
+
+    def __init__(self,
+                 in_channels: int,
+                 intermediate_channels: int,
+                 out_channels: int,
+                 is_2d=False,
+                 **kwargs
+                 ):
         self.in_channels = in_channels
+        self.intermediate_channels = intermediate_channels
         self.out_channels = out_channels
-        self.kernel_sizes = kernel_sizes
+        self.is_2d = is_2d
+
+        conv_op = nn.Conv2d if is_2d else nn.Conv3d
+
+        input_layer = conv_op(
+            in_channels,
+            intermediate_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+        iunet = iUNet(
+            in_channels=intermediate_channels,
+            dim=2 if is_2d else 3,
+        )
+
+        output_layer = conv_op(
+            intermediate_channels,
+            out_channels,
+            kernel_size=3,
+            padding=1
+        )
+
+        self.model = nn.Sequential(input_layer, iunet, output_layer)
 
     def forward(self, xb):
-        raise NotImplementedError('I\'m on strike and I refuse to do any of my work before '
-                                  'Christian does his jobs and implements me!!1!!')
+        self.model(xb)
