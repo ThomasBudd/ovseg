eed2f2d5cff95d40b413dcaeb09f6e2eccb6c40b
ThomasBudd <tb588@cam.ac.uk>
2021-02-19 20:37:21 +0100

    added training of high dose models

--- train_reconstruction_fbp_convs.py
+++ train_reconstruction_fbp_convs.py
@@ -5,30 +5,33 @@ from ovseg.network.recon_networks import get_operator
 import os
 
 
-model_params = get_model_params_2d_reconstruction(image_folder='images_HU_rescale',
-                                                  projection_folder='projections_HU')
-model_params['preprocessing']['window'] = None
+for i, n_photons in enumerate([10 ** 7, 3 * 10 ** 7, 10**8]):
+    dose = 'high_{}'.format(i+1)
+    model_params = get_model_params_2d_reconstruction(image_folder='images_HU_rescale',
+                                                      projection_folder='projections_'+dose)
+    model_params['preprocessing']['window'] = None
+    model_params['preprocessing']['num_photons'] = n_photons
 
-data_name = 'OV04'
-preprocessed_name = 'pod_default'
-val_fold = 0
-if not os.path.exists(os.path.join(os.environ['OV_DATA_BASE'], 'preprocessed', data_name,
-                                   preprocessed_name, 'images_HU_rescale')):
-    op = get_operator()
-    preprocessing = Reconstruction2dSimPreprocessing(op, **model_params['preprocessing'])
-    preprocessing.preprocess_raw_folders(['OV04', 'BARTS', 'ApolloTCGA'],
-                                         preprocessed_name=preprocessed_name,
-                                         data_name=data_name,
-                                         proj_folder_name='projections_HU',
-                                         im_folder_name='images_HU_rescale')
-
-model = Reconstruction2dSimModel(val_fold=val_fold,
-                                 data_name=data_name,
-                                 preprocessed_name=preprocessed_name,
-                                 model_parameters=model_params,
-                                 model_name='recon_fbp_convs_full_HU',
-                                 plot_window=[-50, 350])
-model.training.train()
-
-model.eval_validation_set(save_preds=True)
-model.eval_training_set(save_preds=True)
+    data_name = 'OV04'
+    preprocessed_name = 'pod_default'
+    val_fold = 0
+    if not os.path.exists(os.path.join(os.environ['OV_DATA_BASE'], 'preprocessed', data_name,
+                                       preprocessed_name, 'images_HU_rescale')):
+        op = get_operator()
+        preprocessing = Reconstruction2dSimPreprocessing(op, **model_params['preprocessing'])
+        preprocessing.preprocess_raw_folders(['OV04', 'BARTS', 'ApolloTCGA'],
+                                             preprocessed_name=preprocessed_name,
+                                             data_name=data_name,
+                                             proj_folder_name='projections_'+dose,
+                                             im_folder_name='images_HU_rescale')
+    
+    model = Reconstruction2dSimModel(val_fold=val_fold,
+                                     data_name=data_name,
+                                     preprocessed_name=preprocessed_name,
+                                     model_parameters=model_params,
+                                     model_name='recon_fbp_convs_'+dose,
+                                     plot_window=[-50, 350])
+    model.training.train()
+    
+    model.eval_validation_set(save_preds=True)
+    model.eval_training_set(save_preds=True)
