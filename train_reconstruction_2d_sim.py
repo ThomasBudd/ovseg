932b042878e735f6d633771e1641220780a54ec8
ThomasBudd <tb588@cam.ac.uk>
2021-03-06 20:10:03 +0100

    added parser

--- train_reconstruction_2d_sim.py
+++ train_reconstruction_2d_sim.py
@@ -2,33 +2,52 @@ from ovseg.model.model_parameters_reconstruction import get_model_params_2d_reco
 from ovseg.model.Reconstruction2dSimModel import Reconstruction2dSimModel
 import torch
 import os
-num_epochs = 500
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--data')
+parser.add_argument("-p", "--pretrain_only", required=False, default=False, action='store_true')
+parser.add_argument('--use_windowed_simulations', required=False, default=False, action='store_true')
+
+args = parser.parse_args()
+
+model_name = 'recon_fbp_convs'
+
+if args.use_windowed_simulations:
+    model_name += '_win'
+    projf = 'projections_normal_win'
+    imf = 'images_HU_win_rescale'
+else:
+    projf = 'projections_normal'
+    imf = 'images_HU_rescale'
+
+if args.pretrain_only:
+    model_name += '_pretrained'
+    num_epochs = 500
+else:
+    model_name += '_fully_trained'
+    num_epochs = 1000
 
 val_fold = 0
-data_name = 'OV04'
+data_name = args.data
 
-if not os.path.exists(os.path.join(os.environ['OV_DATA_BASE'], 'preprocessed', data_name,
-                                   'pod_default', 'projections')):
-    from ovseg.preprocessing.Reconstruction2dSimPreprocessing import Reconstruction2dSimPreprocessing
-    from ovseg.networks.recon_networks import get_operator
+if data_name == 'OV04':
+    preprocessed_name = 'pod_default'
+else:
+    preprocessed_name = 'default'
 
-    operator = get_operator()
-    preprocessing = Reconstruction2dSimPreprocessing(operator, window=[-50, 350])
-    preprocessing.preprocess_raw_folders(['OV04', 'BARTS', 'ApolloTCGA'],
-                                         'pod_default',
-                                         data_name=data_name,
-                                         proj_folder_name='projections',
-                                         im_folder_name='images_att')
 
-for architecture in ['reconstruction_network_fbp_convs', 'LPD']:
-    model_parameters = get_model_params_2d_reconstruction(architecture)
-    model_parameters['training']['num_epochs'] = num_epochs
+model_parameters = get_model_params_2d_reconstruction('reconstruction_network_fbp_convs',
+                                                      imf,
+                                                      projf)
+model_parameters['training']['num_epochs'] = num_epochs
 
-    model = Reconstruction2dSimModel(val_fold=val_fold,
-                                     data_name=data_name,
-                                     model_name=architecture,
-                                     model_parameters=model_parameters)
-    model.training.train()
-    model.eval_validation_set(save_preds=True)
-    model.eval_training_set(save_preds=True)
-    torch.cuda.empty_cache()
+model = Reconstruction2dSimModel(val_fold=val_fold,
+                                 data_name=data_name,
+                                 model_name=model_name,
+                                 model_parameters=model_parameters,
+                                 preprocessed_name=preprocessed_name)
+model.training.train()
+model.eval_validation_set(save_preds=True)
+model.eval_training_set(save_preds=True)
+torch.cuda.empty_cache()
