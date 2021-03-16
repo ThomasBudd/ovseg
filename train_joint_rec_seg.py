9d00348b443d01aff1fddf48591446e0017fb8a7
ThomasBudd <tb588@cam.ac.uk>
2021-03-16 15:05:58 +0100

    fixed spacing bug

--- train_joint_rec_seg.py
+++ train_joint_rec_seg.py
@@ -136,8 +136,9 @@ for loss_weight in loss_weights:
     for tpl, scan in tqdm(zip(data.val_ds, data.val_scans)):
         with torch.cuda.amp.autocast():
             recon = model1.predict(tpl, return_torch=True)
-            recon_prep = model2.preprocessing(recon, tpl['spacing'])
-            pred = model2.predict(tpl, model1.pred_key)
+            recon_prep = model2.preprocessing(recon, tpl['orig_spacing'])
+            tpl['image'] = recon_prep
+            pred = model2.predict(tpl)
         case_id = scan.split('.')[0]
         # compute and store results
         results[case_id] = model2.compute_error_metrics(tpl)
