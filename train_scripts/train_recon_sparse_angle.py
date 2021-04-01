01821dec0d8b205012f02fc648564c1a2ed3b9ca
ThomasBudd <tb588@cam.ac.uk>
2021-04-01 21:03:02 +0200

    removed checkpoint bug

--- train_scripts/train_recon_sparse_angle.py
+++ train_scripts/train_recon_sparse_angle.py
@@ -33,7 +33,14 @@ for num_epochs in [500, 1000, 1500, 2000, 2500]:
                                  'OV04',
                                  'recon_fbp_convs_'+str(num_epochs-500),
                                  'fold_0')
-        model.training.load_last_checkpoint(load_path)
+        model_path = os.path.join(os.environ['OV_DATA_BASE'],
+                                  'trained_models',
+                                  'OV04',
+                                  model_name,
+                                  'fold_0')
+        if not os.path.exists(model_path):
+            model.training.load_last_checkpoint(load_path)
+            model.training.save_checkpoint()
     model.training.train()
     model.eval_validation_set(save_preds=True, force_evaluation=False)
     model.eval_training_set(save_preds=True, force_evaluation=False)
