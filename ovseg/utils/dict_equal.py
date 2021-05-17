0ea8ac5b7fb3b918f0fb390d444878934188859c
ThomasBudd <tb588@cam.ac.uk>
2021-05-17 14:45:54 +0100

    removed minor bug

--- ovseg/utils/dict_equal.py
+++ ovseg/utils/dict_equal.py
@@ -13,15 +13,22 @@ def dict_equal(dict1, dict2):
         if not isinstance(item1, type(item2)):
             return False
 
-        if isinstance(item1, np.ndarray):
-            if not np.all(item1 == item2):
-                return False
-
-        elif isinstance(item1, dict):
-            if not dict_equal(item1, item2):
-                return False
-        else:
-            if not item1 == item2:
-                return False
+        try:
+            if isinstance(item1, np.ndarray):
+                if not np.all(item1 == item2):
+                    return False
+            elif isinstance(item1, (list, tuple)):
+                if not np.all(np.array(item1) == np.array(item2)):
+                    return False
+    
+            elif isinstance(item1, dict):
+                if not dict_equal(item1, item2):
+                    return False
+            else:
+                    if not item1 == item2:
+                        return False
+        except ValueError:
+            print('Value Error when compating {} and {}.'.format(item1, item2))
+            return False
 
     return True
