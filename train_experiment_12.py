e11966ba5d57b79a2e1b2d3de0dfefa155247cc3
ThomasBudd <tb588@cam.ac.uk>
2021-06-07 15:14:47 +0100

    added experiments for resrefine networks

--- train_experiment_12.py
+++ train_experiment_12.py
@@ -5,7 +5,6 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument("gpu", type=int)
 parser.add_argument("p", type=int)
 args = parser.parse_args()
 
@@ -18,54 +17,61 @@ exp_list = [[0, 1], [2, 3], [4]][args.p]
 
 def get_model_params(exp):
     # model_name = 'weight_decay_{:.1e}'.format(weight_decay)
-    model_name = 'res_refine'
+    model_name = ['res_refine', 'res_refine_no_gv_augs', 'label_aug_rare', 'label_aug_freq',
+                  'res_refine_500'][exp]
 
     patch_size = [20, 160, 160]
-    prg_trn_sizes = [[12, 192, 192],
-                     [15, 240, 240],
-                     [17, 272, 272],
-                     [20, 320, 320]]
-    out_shape = [[12, 96, 96],
-                 [15, 120, 120],
-                 [17, 136, 136],
-                 [20, 160, 160]]
     model_params = get_model_params_3d_cascade('pod_half',
                                                'res_encoder_p_bias_0.5',
                                                patch_size,
                                                n_2d_convs=3,
                                                use_prg_trn=False)
-    model_params['training']['prg_trn_sizes'] = prg_trn_sizes
 
-    del model_params['network']['kernel_sizes']
-    del model_params['network']['kernel_sizes_up']
-    del model_params['network']['n_pyramid_scales']
-    model_params['architecture'] = 'unetresencoder'
+    del model_params['network']
+    model_params['architecture'] = 'refineresnet'
     model_params['data']['trn_dl_params']['min_biased_samples'] = 0
     model_params['data']['val_dl_params']['min_biased_samples'] = 0
     model_params['data']['trn_dl_params']['p_bias_sampling'] = 0.5
     model_params['data']['val_dl_params']['p_bias_sampling'] = 0.5
-    model_params['network']['block'] = 'res'
-    model_params['network']['z_to_xy_ratio'] = 8
-    model_params['network']['stochdepth_rate'] = 0
-    # this time we change the amount of augmentation during training
-    prg_trn_aug_params = {}
-    c = 4
+    model_params['data']['trn_dl_params']['bias']='mv'
+    model_params['data']['val_dl_params']['bias']='mv'
+    model_params['network']= {'in_channels': 2, 'out_channels': 2,'hid_channels': 32, 
+                              'z_to_xy_ratio': 8}
 
-    prg_trn_aug_params['mm_var_noise'] = np.array([[0, 0.1/c], [0, 0.1]])
-    prg_trn_aug_params['mm_sigma_blur'] = np.array([[0.5/c, 0.5 + 1/c], [0.5, 1.5]])
-    prg_trn_aug_params['mm_bright'] = np.array([[1 - 0.3/c, 1 + 0.3/c], [0.7, 1.3]])
-    prg_trn_aug_params['mm_contr'] = np.array([[1 - 0.35/c, 1 + 0.5/c], [0.65, 1.5]])
-    prg_trn_aug_params['mm_low_res'] = np.array([[1, 1 + 1/c], [1, 2]])
-    prg_trn_aug_params['mm_gamma'] = np.array([[1 - 0.3/c, 1 + 0.5/c], [0.7, 1.5]])
-    prg_trn_aug_params['out_shape'] = out_shape
-    model_params['training']['prg_trn_aug_params'] = prg_trn_aug_params
-    model_params['training']['prg_trn_resize_on_the_fly'] = False
+    if exp == 1:
+        del model_params['augmentation']['torch_params']['grayvalue']
+
+    if exp == 2:
+        model_params['augmentation']['np_params'] = {'mask': {'p_morph': 0.4,
+                                                              'radius_mm': [0, 2],
+                                                              'p_removal': 0.0,
+                                                              'vol_percentage_removal': 0.15,
+                                                              'vol_threshold_removal': None,
+                                                              'threeD_morph_ops': False,
+                                                              'aug_channels': [1]}}
+    elif exp == 3:
+        model_params['augmentation']['np_params'] = {'mask': {'p_morph': 1.0,
+                                                              'radius_mm': [0, 2],
+                                                              'p_removal': 0.0,
+                                                              'vol_percentage_removal': 0.15,
+                                                              'vol_threshold_removal': None,
+                                                              'threeD_morph_ops': False,
+                                                              'aug_channels': [1]}}
+    else:
+        del model_params['augmentation']['np_params']
+    if exp == 4:
+        model_params['training']['num_epochs'] = 500
+        n_warmup_epochs = 25
+    else:
+        model_params['training']['num_epochs'] = 250
+        n_warmup_epochs = 13
+        
+
     model_params['training']['lr_schedule'] = 'lin_ascent_cos_decay'
-    model_params['training']['lr_params'] = {'n_warmup_epochs': 50, 'lr_max': 0.02}
-    model_params['training']['opt_params'] = {'momentum': 0.99,
-                                              'weight_decay': 3e-5,
-                                              'nesterov': True,
-                                              'lr': 2*10**-2}
+    model_params['training']['lr_params'] = {'n_warmup_epochs': n_warmup_epochs,
+                                             'lr_max': 3 * 10**-4}
+    model_params['training']['opt_name'] = 'Adam'
+    model_params['training']['opt_params'] = {'lr': 3 * 10**-4}
     return model_params, model_name
 
 
@@ -77,7 +83,7 @@ for val_fold, exp in zip(val_fold_list, exp_list):
                               model_name=model_name,
                               model_parameters=model_params)
     model.training.train()
-    model.eval_validation_set()
+    model.eval_training_set()
     model.eval_raw_data_npz('BARTS')
     model.clean()
     
