9e46f43a4cc15120d4e7e858d20be0f23b9334ed
ThomasBudd <tb588@cam.ac.uk>
2021-07-06 15:39:18 +0200

    added 2d sequential training and BARTS perprocessing

--- preprocess_dose_series_restauration.py
+++ preprocess_dose_series_restauration.py
@@ -1,4 +1,5 @@
 from ovseg.preprocessing.Restauration2dSimPreprocessing import Restauration2dSimPreprocessing
+from ovseg.preprocessing.SegmentationPreprocessing import SegmentationPreprocessing
 import numpy as np
 
 window = [-32, 318]
@@ -23,7 +24,23 @@ for dose_level, ext in zip(dl_list, ext_list):
     fbp_folder_name = 'fbps_'+ext
     im_folder_name = 'images_restauration'
 
-    preprocesseing.preprocess_raw_folders('OV04', 'pod_2d',
+    # preprocesseing.preprocess_raw_folders('OV04', 'pod_2d',
+    #                                       fbp_folder_name=fbp_folder_name,
+    #                                       im_folder_name=im_folder_name,
+    #                                       save_as_fp16=True)
+    preprocesseing.preprocess_raw_folders('BARTS', 'pod_2d',
+                                          data_name='OV04',
                                           fbp_folder_name=fbp_folder_name,
                                           im_folder_name=im_folder_name,
                                           save_as_fp16=True)
+preprocesseing = SegmentationPreprocessing(apply_resizing=False,
+                                           apply_pooling=False,
+                                           apply_windowing=True,
+                                           window=window,
+                                           scaling=scaling,
+                                           lb_classes=[9])
+
+
+# preprocesseing.preprocess_raw_folders('OV04', 'pod_2d')
+preprocesseing.preprocess_raw_folders('BARTS', 'pod_2d', data_name='OV04')
+
---
+++ train_2d_sequential.py
@@ -0,0 +1,28 @@
+from ovseg.model.SegmentationModel import SegmentationModel
+from ovseg.model.model_parameters_segmentation import get_model_params_2d_segmentation
+import nibabel as nib
+import numpy as np
+from os import environ, mkdir, listdir
+from os.path import join, exists
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("exp", type=int)
+parser.add_argument("run", type=int)
+args = parser.parse_args()
+
+im_folder = ['images', 'restaurations_full', 'restaurations_half', 'restaurations_quater',
+             'restaurations_eights', 'restaurations_16', 'restaurations_32'][args.exp]
+
+
+model_params = get_model_params_2d_segmentation()
+model_params['data']['folders'][0] = im_folder
+model = SegmentationModel(val_fold=5+args.run,
+                          data_name='OV04',
+                          model_name='2d_sequential_'+im_folder,
+                          model_parameters=model_params,
+                          preprocessed_name='pod_2d')
+
+model.training.train()
+model.eval_validation_set(save_preds=True)
+
