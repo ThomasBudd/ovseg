6bbee12fbef90b35afc8aa61223bd71074089eb9
ThomasBudd <tb588@cam.ac.uk>
2021-07-13 22:25:03 +0200

    updated regionfinding

--- preprocess_regionfinding.py
+++ preprocess_regionfinding.py
@@ -1,5 +1,5 @@
 from ovseg.preprocessing.RegionfindingPreprocessing import RegionfindingPreprocessing
 
-prep = RegionfindingPreprocessing(True, False, True)
+prep = RegionfindingPreprocessing(True, False, True, mask_dist=[2, 15, 15])
 prep.plan_preprocessing_raw_data('OV04')
 prep.preprocess_raw_data('OV04', 'multiclass')
