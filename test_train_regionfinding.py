1b5af929d5a0e3596aa3eb97a32a462f3118648e
ThomasBudd <tb588@cam.ac.uk>
2021-07-15 19:49:03 +0200

    removed some bug for the region finding

--- test_train_regionfinding.py
+++ test_train_regionfinding.py
@@ -1,6 +1,11 @@
 from ovseg.model.RegionfindingModel import RegionfindingModel
 from ovseg.model.model_parameters_segmentation import get_model_params_3d_res_encoder_U_Net
 
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("gpu", type=int)
+args = parser.parse_args()
+
 
 model_params = get_model_params_3d_res_encoder_U_Net([32, 256, 256], 8, n_fg_classes=11,
                                                      larger_res_encoder=True)
@@ -12,14 +17,15 @@ model_params['training']['loss_params'] = {'loss_names': ['cross_entropy_weighte
 model_params['training']['batches_have_masks'] = True
 model_params['data']['folders'] = ['images', 'labels', 'masks']
 model_params['data']['keys'] = ['image', 'label', 'mask']
-
+model_params['data']['trn_dl_params']['mask_key'] = 'mask'
+model_params['data']['val_dl_params']['mask_key'] = 'mask'
 
 # model_params['data']['trn_dl_params']['epoch_len'] = 25
 # model_params['data']['val_dl_params']['epoch_len'] = 2
 # model_params['network']['filters'] = 8
 # model_params['training']['num_epochs'] = 100
 
-model = RegionfindingModel(val_fold=0, data_name='OV04', preprocessed_name='multiclass',
+model = RegionfindingModel(val_fold=args.gpu, data_name='OV04', preprocessed_name='multiclass',
                            model_name='ROIfinding_p01', model_parameters=model_params)
 
 model.training.train()
