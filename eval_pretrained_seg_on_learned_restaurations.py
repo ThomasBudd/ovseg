d4cc3e793e9142fbb3fdb6d0c85f6cee77c60709
ThomasBudd <tb588@cam.ac.uk>
2021-09-07 16:27:25 +0200

    removed bug

--- eval_pretrained_seg_on_learned_restaurations.py
+++ eval_pretrained_seg_on_learned_restaurations.py
@@ -3,30 +3,30 @@ from ovseg.model.SegmentationModel import SegmentationModel
 from ovseg.model.model_parameters_segmentation import get_model_params_2d_segmentation
 from ovseg.data.Dataset import Dataset
 import torch
-
+import pickle
 
 model_params = get_model_params_2d_segmentation()
 del model_params['augmentation']['torch_params']['grayvalue']
 model_params['network']['norm'] = 'inst'
-model_name = 'delete_me_2d_joint_'+'full'
+model_name = '2d_num_epochs_ADAM_long'
 
 # this is just creating a new blank segmentation model with random weights
-seg_model = SegmentationModel(val_fold=5,
-                              data_name='OV04',
-                              model_name=model_name,
-                              model_parameters=model_params,
-                              preprocessed_name='pod_2d',
-                              dont_store_data_in_ram=True)
 # %% creat datasets
 keys = ['image', 'label']
 folders_full = ['restaurations_full', 'labels']
 folders_quater = ['restaurations_quater', 'labels']
 preprocessed_path = os.path.join(os.environ['OV_DATA_BASE'], 'preprocessed', 'OV04', 'pod_2d')
-scans = seg_model.data.val_scans
+splits = pickle.load(open(os.path.join(preprocessed_path, 'splits.pkl'), 'rb'))
+scans = splits[-1]['val']
 ds_full = Dataset(scans, preprocessed_path, keys, folders_full)
 ds_quater = Dataset(scans, preprocessed_path, keys, folders_quater)
 
 for f in range(5, 8):
+    seg_model = SegmentationModel(val_fold=f,
+                                  data_name='OV04',
+                                  model_name=model_name,
+                                  preprocessed_name='pod_2d',
+                                  dont_store_data_in_ram=True)
     # load pretrained weights
     path_to_weights = os.path.join(os.environ['OV_DATA_BASE'],
                                    'trained_models',
