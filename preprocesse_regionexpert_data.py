c54845b938d9cce24bdf1d83dbe5655b0fb814df
ThomasBudd <tb588@cam.ac.uk>
2021-09-14 14:03:48 +0200

    added target spacing

--- preprocesse_regionexpert_data.py
+++ preprocesse_regionexpert_data.py
@@ -19,7 +19,8 @@ for w in weights:
                                      region_finding_model={'data_name': 'OV04',
                                                            'preprocessed_name': 'pod_om_reg',
                                                            'model_name': 'regfinding_{}'.format(w)},
-                                     lb_classes=[cl])
+                                     lb_classes=[cl],
+                                     target_spacing=[5.0, 0.8, 0.8])
     
     prep.plan_preprocessing_raw_data('OV04')
     prep.preprocess_raw_data('OV04', pref+'_reg_expert_{}'.format(w))
