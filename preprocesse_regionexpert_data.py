c21b1d90b2a2a68d08e9ca8318875ce707991b21
ThomasBudd <tb588@cam.ac.uk>
2021-09-20 15:08:58 +0200

    prepared new experiments

--- preprocesse_regionexpert_data.py
+++ preprocesse_regionexpert_data.py
@@ -2,25 +2,23 @@ from ovseg.preprocessing.RegionexpertPreprocessing import RegionexpertPreprocess
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--om", default=False, action='store_true')
 args = parser.parse_args()
 
-cl = 1 if args.om else 9
+classes = [2, 15]
 
-pref = 'om' if args.om else 'pod'
+prefs = ['liver', 'diaph']
 
-weights = [0.01, 0.03, 0.1, 0.3]
+w = 0.3
 
-
-for w in weights:
+for cl, pref in zip(classes, prefs):
     prep = RegionexpertPreprocessing(apply_resizing=True,
                                      apply_pooling=False,
                                      apply_windowing=True,
                                      region_finding_model={'data_name': 'OV04',
-                                                           'preprocessed_name': 'pod_om_reg',
+                                                           'preprocessed_name': 'multiclass_reg',
                                                            'model_name': 'regfinding_{}'.format(w)},
                                      lb_classes=[cl],
                                      target_spacing=[5.0, 0.8, 0.8])
     
     prep.plan_preprocessing_raw_data('OV04')
-    prep.preprocess_raw_data('OV04', pref+'_reg_expert_{}'.format(w))
+    prep.preprocess_raw_data('OV04', pref+'_reg_expert')
