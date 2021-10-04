5e4f3977d10b4f3e643185f1abf2fae7d07ca511
ThomasBudd <tb588@cam.ac.uk>
2021-10-04 23:10:23 +0200

    added preprocessing for SLDS expert

--- preprocess_SLDS_expert.py
+++ preprocess_SLDS_expert.py
@@ -1,14 +1,14 @@
-from ovseg.preprocessing.RegionexpertPreprocessing import RegionexpertPreprocessing
+from ovseg.preprocessing.SLDSExpertPreprocessing import SLDSExpertPreprocessing
 import argparse
 
 
-prep = RegionexpertPreprocessing(apply_resizing=True,
-                                 apply_pooling=False,
-                                 apply_windowing=True,
-                                 region_finding_model={'data_name': 'OV04',
-                                                       'preprocessed_name': 'SLDS',
-                                                       'model_name': 'U-Net5_0.01'},
-                                 lb_classes=[2])
+prep = SLDSExpertPreprocessing(apply_resizing=True,
+                               apply_pooling=False,
+                               apply_windowing=True,
+                               region_finding_model={'data_name': 'OV04',
+                                                     'preprocessed_name': 'SLDS',
+                                                     'model_name': 'U-Net5_0.01'},
+                               lb_classes=[2])
 
 prep.plan_preprocessing_raw_data('OV04')
 prep.preprocess_raw_data('OV04', 'SLDS_reg_expert_0.01')
