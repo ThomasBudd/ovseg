15c6ddb6b169819be4cc299b01452eadc1de1cbb
ThomasBudd <tb588@cam.ac.uk>
2021-10-06 12:17:19 +0200

    updated

--- check_regions_SLDS.py
+++ check_regions_SLDS.py
@@ -17,10 +17,14 @@ for w in w_list:
                  'U-Net5_'+str(w), 'cross_validation')
     n_reg = 0
     n_fg_reg = 0
+    reg_sizes = []
+    size = 0
     sleep(0.5)
     print()
     for case in tqdm(listdir(predp)):
-        gt = (nib.load(join(gtp, case)).get_fdata() > 0).astype(float)
+        img = nib.load(join(gtp, case))
+        fac = np.prod(img.header['pixdim'][1:4]) /1000
+        gt = (img.get_fdata() > 0).astype(float)
         regs = (nib.load(join(predp, case)).get_fdata() == 2).astype(float)
         comps = label(regs)
         comps_gpu = torch.from_numpy(comps).cuda()
@@ -28,11 +32,21 @@ for w in w_list:
         n_reg += comps.max()
         for c in range(1, comps.max() + 1):
             comp = (comps_gpu == c).type(torch.float)
-            n_fg_reg += (comp * gt_gpu).max().item()
+            comp_size = comp.sum().item() * fac
+            reg_sizes.append(comp_size)
+            has_fg = (comp * gt_gpu).max().item()
+            n_fg_reg += has_fg
+            if has_fg == 0:
+                size = np.max([size, comp_size])
+    
+    reg_sizes = np.array(reg_sizes)
+    p = 100 * np.mean(reg_sizes <= size)
+    
     print()
     print('w:{}, produces {} regions of which {:.2f}% showed fg'.format(w,
                                                                         n_reg,
                                                                         100 * n_fg_reg/n_reg))
+    print('The largest region without fg was {:.2f}cm3, {:.2f}% of the regions can be discarded'.format(size, p))
     print()
 
 
@@ -41,12 +55,17 @@ gtp = join(environ['OV_DATA_BASE'], 'raw_data', 'BARTS', 'labels')
 for w in w_list:
     predp = join(environ['OV_DATA_BASE'], 'predictions', 'OV04', 'SLDS',
                  'U-Net5_'+str(w), 'BARTS_ensemble_0_1_2_3_4')
+    
     n_reg = 0
     n_fg_reg = 0
+    reg_sizes = []
+    size = 0
     sleep(0.5)
     print()
     for case in tqdm(listdir(predp)):
-        gt = (nib.load(join(gtp, case)).get_fdata() > 0).astype(float)
+        img = nib.load(join(gtp, case))
+        fac = np.prod(img.header['pixdim'][1:4]) /1000
+        gt = (img.get_fdata() > 0).astype(float)
         regs = (nib.load(join(predp, case)).get_fdata() == 2).astype(float)
         comps = label(regs)
         comps_gpu = torch.from_numpy(comps).cuda()
@@ -54,9 +73,19 @@ for w in w_list:
         n_reg += comps.max()
         for c in range(1, comps.max() + 1):
             comp = (comps_gpu == c).type(torch.float)
-            n_fg_reg += (comp * gt_gpu).max().item()
+            comp_size = comp.sum().item() * fac
+            reg_sizes.append(comp_size)
+            has_fg = (comp * gt_gpu).max().item()
+            n_fg_reg += has_fg
+            if has_fg == 0:
+                size = np.max([size, comp_size])
+    
+    reg_sizes = np.array(reg_sizes)
+    p = 100 * np.mean(reg_sizes <= size)
+    
     print()
     print('w:{}, produces {} regions of which {:.2f}% showed fg'.format(w,
                                                                         n_reg,
                                                                         100 * n_fg_reg/n_reg))
+    print('The largest region without fg was {:.2f}cm3, {:.2f}% of the regions can be discarded'.format(size, p))
     print()
--- eval_SLDS.py
+++ eval_SLDS.py
@@ -11,6 +11,7 @@ w_list = [0.001, 0.01, 0.1]
 for w in w_list:
     
     dscs = []
+    sens_15 = []
     p1 = join(environ['OV_DATA_BASE'], 'predictions', 'OV04', 'SLDS', 'U-Net5_{}'.format(w),
               'BARTS_ensemble_0_1_2_3_4')
     p2 = join(environ['OV_DATA_BASE'], 'predictions', 'OV04', 'SLDS_reg_expert_{}'.format(w),
@@ -18,7 +19,9 @@ for w in w_list:
     
     sleep(0.5)
     for case in tqdm(listdir(p1)):
-        gt = (nib.load(join(gtp, case)).get_fdata() > 0).astype(float)
+        gt = nib.load(join(gtp, case)).get_fdata()
+        gt_bin = (gt > 0).astype(float)
+        gt_15 = (gt == 15).astype(float)
         pred1 = (nib.load(join(gtp, case)).get_fdata() == 1).astype(float)
         pred2 = (nib.load(join(gtp, case)).get_fdata() > 0 ).astype(float)
         pred = pred1 + pred2
@@ -26,6 +29,9 @@ for w in w_list:
         if pred.max() > 1:
             print('We got overlap for case {}'.format(case))
         
-        if gt.max() > 0:
-            dscs.append(200 * np.sum(gt * pred) / np.sum(gt + pred))
-    print('w: {}, mean dsc: {}'.format(w, np.mean(dscs)))
+        if gt_bin.max() > 0:
+            dscs.append(200 * np.sum(gt_bin * pred) / np.sum(gt_bin + pred))
+        
+        if gt_15.max() > 0:
+            sens_15.append(100 * np.sum(gt_15 * pred) / np.sum(gt_15 + pred))
+    print('w: {}, mean dsc: {:.2f}, sens 15: {:.2f}'.format(w, np.mean(dscs), np.mean(sens_15)))
