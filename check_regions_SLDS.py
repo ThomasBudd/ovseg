e83c0e6c5fbe947b9175d58b539c5c95e50858fe
ThomasBudd <tb588@cam.ac.uk>
2021-10-06 09:48:29 +0200

    new features for SLDS evaluation

--- check_regions_SLDS.py
+++ check_regions_SLDS.py
@@ -18,6 +18,7 @@ for w in w_list:
     n_reg = 0
     n_fg_reg = 0
     sleep(0.5)
+    print()
     for case in tqdm(listdir(predp)):
         gt = (nib.load(join(gtp, case)).get_fdata() > 0).astype(float)
         regs = (nib.load(join(predp, case)).get_fdata() == 2).astype(float)
@@ -28,7 +29,34 @@ for w in w_list:
         for c in range(1, comps.max() + 1):
             comp = (comps_gpu == c).type(torch.float)
             n_fg_reg += (comp * gt_gpu).max().item()
-    
+    print()
     print('w:{}, produces {} regions of which {:.2f}% showed fg'.format(w,
                                                                         n_reg,
                                                                         100 * n_fg_reg/n_reg))
+    print()
+
+
+gtp = join(environ['OV_DATA_BASE'], 'raw_data', 'BARTS', 'labels')
+
+for w in w_list:
+    predp = join(environ['OV_DATA_BASE'], 'predictions', 'OV04', 'SLDS',
+                 'U-Net5_'+str(w), 'BARTS_ensemble_0_1_2_3_4')
+    n_reg = 0
+    n_fg_reg = 0
+    sleep(0.5)
+    print()
+    for case in tqdm(listdir(predp)):
+        gt = (nib.load(join(gtp, case)).get_fdata() > 0).astype(float)
+        regs = (nib.load(join(predp, case)).get_fdata() == 2).astype(float)
+        comps = label(regs)
+        comps_gpu = torch.from_numpy(comps).cuda()
+        gt_gpu = torch.from_numpy(gt).cuda()
+        n_reg += comps.max()
+        for c in range(1, comps.max() + 1):
+            comp = (comps_gpu == c).type(torch.float)
+            n_fg_reg += (comp * gt_gpu).max().item()
+    print()
+    print('w:{}, produces {} regions of which {:.2f}% showed fg'.format(w,
+                                                                        n_reg,
+                                                                        100 * n_fg_reg/n_reg))
+    print()
