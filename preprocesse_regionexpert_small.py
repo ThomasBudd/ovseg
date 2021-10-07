52167255188102386f1d607ceacfb1b66afb2ce4
ThomasBudd <tb588@cam.ac.uk>
2021-10-07 09:43:13 +0200

    added regionexperts for small lesions

--- preprocesse_regionexpert_small.py
+++ preprocesse_regionexpert_small.py
@@ -14,7 +14,8 @@ if args.reg == 0:
                                                            'preprocessed_name': 'mesentery_reg',
                                                            'model_name': 'regfinding_0.1'},
                                      lb_classes=[5],
-                                     target_spacing=[5.0, 0.67, 0.67])
+                                     target_spacing=[5.0, 0.67, 0.67],
+                                     save_only_fg_scans=False)
     
     prep.plan_preprocessing_raw_data('OV04')
     prep.preprocess_raw_data('OV04', 'mesentery_reg_expert')
@@ -28,7 +29,8 @@ else:
                                                            'model_name': 'regfinding_0.1'},
                                      lb_classes=[3,4,5,6,7,11,12,13,14,15,16,17,18],
                                      reduce_lb_to_single_class=True,
-                                     target_spacing=[5.0, 0.67, 0.67])
+                                     target_spacing=[5.0, 0.67, 0.67],
+                                     save_only_fg_scans=False)
     
     prep.plan_preprocessing_raw_data('OV04')
     prep.preprocess_raw_data('OV04', 'small_reg_expert')
