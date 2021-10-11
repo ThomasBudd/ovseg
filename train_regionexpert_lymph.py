25a5c6aa016f99848305d3a445564e01415a6f6d
ThomasBudd <tb588@cam.ac.uk>
2021-10-11 19:23:41 +0200

    updated script

--- train_regionexpert_lymph.py
+++ train_regionexpert_lymph.py
@@ -42,3 +42,32 @@ model.training.train()
 # model.eval_validation_set()
 # model.eval_training_set(force_evaluation=True)
 model.eval_raw_dataset('BARTS')
+
+
+model_name = 'U-Net_resencoder'
+
+model_params = get_model_params_3d_res_encoder_U_Net(patch_size=[8, 48, 48],
+                                                     z_to_xy_ratio=5.0/0.67,
+                                                     use_prg_trn=False,
+                                                     larger_res_encoder=False,
+                                                     n_fg_classes=1)
+model_params['network']['filters'] = 32
+model_params['network']['n_blocks_list'] = [6, 3]
+model_params['training']['loss_params'] = {'loss_names': ['cross_entropy',
+                                                          'dice_loss']}
+
+model_params['data']['folders'] = ['images', 'labels', 'regions']
+model_params['data']['keys'] = ['image', 'label', 'region']
+model_params['training']['batches_have_masks'] = True
+model_params['training']['num_epochs'] = 500
+model_params['postprocessing'] = {'mask_with_reg': True}
+
+model = RegionexpertModel(val_fold=0,
+                          data_name='OV04',
+                          preprocessed_name=p_name, 
+                          model_name=model_name,
+                          model_parameters=model_params)
+model.training.train()
+# model.eval_validation_set()
+# model.eval_training_set(force_evaluation=True)
+model.eval_raw_dataset('BARTS')
