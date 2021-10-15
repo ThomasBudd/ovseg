0022214366f22bae46bf6913d6bdaa4563f477c0
ThomasBudd <tb588@cam.ac.uk>
2021-10-15 10:57:17 +0200

    updated scripts

--- train_regionfinding_multiclass.py
+++ train_regionfinding_multiclass.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("vf", type=int)
 args = parser.parse_args()
 
-w = 0
+w = 0.001
 vf = args.vf
 
 p_name = 'multiclass_reg'
@@ -29,10 +29,10 @@ model_params = get_model_params_3d_res_encoder_U_Net(patch_size,
 
 model_params['training']['loss_params'] = {'loss_names': ['cross_entropy_weighted_bg',
                                                           'dice_loss_weighted'],
-                                          'loss_kwargs': [{'weight_bg': 0,
+                                          'loss_kwargs': [{'weight_bg': w,
                                                            'n_fg_classes': 6},
                                                           {'eps': 1e-5,
-                                                           'weight': 0}]}
+                                                           'weight': w}]}
 model_params['data']['folders'] = ['images', 'labels', 'regions']
 model_params['data']['keys'] = ['image', 'label', 'region']
 # we train using the regions as ground truht we're training for
@@ -40,8 +40,6 @@ for dl_str in ['trn_dl_params', 'val_dl_params']:
     model_params['data'][dl_str]['label_key'] = 'region'
     model_params['data'][dl_str]['bias'] = 'cl_fg'
     model_params['data'][dl_str]['n_fg_classes'] = 6
-    model_params['data'][dl_str]['min_biased_samples'] = 2
-model_params['network']['use_logit_bias'] = True
 
 model = RegionfindingModel(val_fold=vf,
                            data_name='OV04',
@@ -66,33 +64,19 @@ model.eval_raw_data_npz('BARTS')
 ens = RegionfindingEnsemble(val_fold=list(range(5)), 
                             data_name='OV04',
                             preprocessed_name=p_name,
-                            model_name='regfinding_0')
+                            model_name='regfinding_'+str(w))
 if ens.all_folds_complete():
     ens.eval_raw_dataset('BARTS', save_preds=True)
 
-    prep = RegionexpertPreprocessing(apply_resizing=True,
-                                     apply_pooling=False,
-                                     apply_windowing=True,
-                                     region_finding_model={'data_name': 'OV04',
-                                                           'preprocessed_name': p_name,
-                                                           'model_name': 'regfinding_'+str(w)},
-                                     lb_classes=[13, 15, 17],
-                                     target_spacing=[5.0, 0.67, 0.67],
-                                     save_only_fg_scans=False)
-    
-    prep.plan_preprocessing_raw_data('OV04')
-    prep.preprocess_raw_data('OV04', 'lymph_reg_expert_'+str(w))
-
-    prep = RegionexpertPreprocessing(apply_resizing=True,
-                                     apply_pooling=False,
-                                     apply_windowing=True,
-                                     region_finding_model={'data_name': 'OV04',
-                                                           'preprocessed_name': p_name,
-                                                           'model_name': 'regfinding_'+str(w)},
-                                     lb_classes=[1],
-                                     target_spacing=[5.0, 0.67, 0.67],
-                                     save_only_fg_scans=False)
-    
-    prep.plan_preprocessing_raw_data('OV04')
-    prep.preprocess_raw_data('OV04', 'om_reg_expert_'+str(w))
-
+    for lb_classes in [[1], [2], [9], [13, 15, 17]]:
+        prep = RegionexpertPreprocessing(apply_resizing=True,
+                                         apply_pooling=False,
+                                         apply_windowing=True,
+                                         region_finding_model={'data_name': 'OV04',
+                                                               'preprocessed_name': p_name,
+                                                               'model_name': 'regfinding_'+str(w)},
+                                         lb_classes=lb_classes,
+                                         target_spacing=[5.0, 0.67, 0.67],
+                                         save_only_fg_scans=False)
+        prep.plan_preprocessing_raw_data('OV04')
+        prep.preprocess_raw_data('OV04', 'reg_expert_'+'_'.join([str(c) for c in lb_classes]))
