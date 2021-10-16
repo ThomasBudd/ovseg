3600ce1e43d90308c052c82325e21ec90d7bc97a
ThomasBudd <tb588@cam.ac.uk>
2021-10-17 00:08:50 +0200

    updated script

---
+++ check_and_export_13_15_17.py
@@ -0,0 +1,69 @@
+import numpy as np
+import nibabel as nib
+from ovseg.utils.io import read_dcms, save_dcmrt_from_data_tpl
+from ovseg.utils.label_utils import reduce_classes
+from os import environ, listdir
+from os.path import join, basename
+from tqdm import tqdm
+import pickle
+from skimage.transform import resize
+# %%
+predp = join(environ['OV_DATA_BASE'], 'predictions', 'OV04', 'multiclass_13_15_17', 'U-Net5',
+             'BARTS_ensemble_0_1_2_3_4')
+gtp = join(environ['OV_DATA_BASE'], 'raw_data', 'BARTS', 'labels')
+
+lb_classes = [13, 15, 17]
+dscs = np.zeros(3)
+fps = np.zeros(3)
+has_fgs = np.zeros(3)
+
+for case in tqdm(listdir(predp)):
+    
+    gt = nib.load(join(gtp, case)).get_fdata()
+    pred = nib.load(join(predp, case)).get_fdata()
+
+    for i, cl in enumerate(lb_classes):
+        l1 = (gt == cl).astype(float)
+        l2 = (pred == cl).astype(float)
+        
+        has_fg = l1.max()
+        has_fgs[i] += has_fg
+        
+        if has_fg:
+            dscs[i] += 200 * np.sum(l1*l2)/np.sum(l1 + l2)
+        else:
+            fps[i] += l2.max()
+    
+
+dscs = dscs/has_fgs
+n = len(listdir(predp))
+fps = 100 * fps / (n - has_fg)
+
+for i in range(3):
+    print('{}: DSC: {:.2f}, fps: {:.2f}'.format(lb_classes[i],
+                                                dscs[i],
+                                                fps[i]))
+    
+# %% now export to dcm rt
+data_info = pickle.load(open(join(environ['OV_DATA_BASE'], 'raw_data', 'BARTS', 'data_info.pkl'), 'rb'))
+
+gtp_dcm = join(environ['OV_DATA_BASE'], 'raw_data', 'BARTS_dcm')
+for case in tqdm(listdir(predp)):
+    pred = nib.load(join(predp, case)).get_fdata()
+    if pred.max() == 0:
+        continue
+    
+    pred = reduce_classes(pred, [13, 15, 17])
+    pred = np.swapaxes(pred, -1, 0)
+    info = data_info[case[5:8]]
+    data_tpl = read_dcms(join(gtp_dcm, info['scan']))
+    
+    if data_tpl['label'].shape[0] != pred.shape[0]:
+        pred = resize(pred, data_tpl['label'].shape, order=0)
+    
+    data_tpl['prediction'] = pred
+    out_file = join(predp, 'dcmrt', basename(data_tpl['raw_label_file']))
+    save_dcmrt_from_data_tpl(data_tpl, out_file, key='prediction', names=['13', '15', '17'])
+    
+    
+    
--- train_regionexpert_multiclass.py
+++ train_regionexpert_multiclass.py
@@ -8,10 +8,9 @@ parser = argparse.ArgumentParser()
 parser.add_argument("vf", type=int)
 args = parser.parse_args()
 
-w = 0
+w = 0.001
 vf = args.vf
 
-p_name = 'om_reg_expert_0'
 patch_size = [40, 320, 320]
 use_prg_trn = True
 out_shape = [[24, 192, 192],
@@ -19,36 +18,44 @@ out_shape = [[24, 192, 192],
              [36, 288, 288],
              [40, 320, 320]]
 larger_res_encoder = True
-model_params = get_model_params_3d_res_encoder_U_Net(patch_size, 
-                                                     5/0.67,
-                                                     n_fg_classes=1,
-                                                     use_prg_trn=use_prg_trn,
-                                                     larger_res_encoder=larger_res_encoder,
-                                                     out_shape=out_shape)
-model_params['training']['loss_params'] = {'loss_names': ['cross_entropy',
-                                                          'dice_loss']}
-model_params['data']['folders'] = ['images', 'labels', 'regions']
-model_params['data']['keys'] = ['image', 'label', 'region']
-model_params['training']['batches_have_masks'] = True
-model_params['training']['num_epochs'] = 1000
-model_params['postprocessing'] = {'mask_with_reg': True}
-model = RegionexpertModel(val_fold=args.vf,
-                          data_name='OV04',
-                          preprocessed_name=p_name, 
-                          model_name='U-Net5',
-                          model_parameters=model_params)
-model.training.train()
-model.eval_validation_set()
-model.eval_raw_data_npz('BARTS')
 
-if vf == 0:
-    ens = RegionexpertEnsemble(val_fold=list(range(5)),
-                               data_name='OV04',
-                               model_name='U-Net5',
-                               preprocessed_name=p_name)
 
-    while not ens.all_folds_complete():
-        print('Wait 1 min...')
-        sleep(60)
+lb_classes_list = [[1], [2], [9], [13, 15, 17]]
+
+for lb_classes in lb_classes_list:
+
+    model_params = get_model_params_3d_res_encoder_U_Net(patch_size=patch_size,
+                                                         z_to_xy_ratio=5.0/0.67,
+                                                         use_prg_trn=use_prg_trn,
+                                                         larger_res_encoder=larger_res_encoder,
+                                                         n_fg_classes=len(lb_classes),
+                                                         out_shape=out_shape)
+    model_params['training']['loss_params'] = {'loss_names': ['cross_entropy',
+                                                              'dice_loss']}
+    model_params['data']['val_dl_params']['n_fg_classes'] = len(lb_classes)
+    model_params['data']['trn_dl_params']['n_fg_classes'] = len(lb_classes)
+    model_params['data']['val_dl_params']['bias'] = 'cl_fg'
+    model_params['data']['trn_dl_params']['bias'] = 'cl_fg'
     
-    ens.eval_raw_dataset('BARTS')
+    model_name = 'U-Net5'
+    p_name = 'reg_expert_'+'_'.join([str(c) for c in lb_classes])
+    model = RegionexpertModel(val_fold=args.vf,
+                              data_name='OV04',
+                              preprocessed_name=p_name, 
+                              model_name=model_name,
+                              model_parameters=model_params)
+    model.training.train()
+    model.eval_validation_set()
+
+lb_classes = lb_classes_list[args.vf]
+p_name = 'reg_expert_'+'_'.join([str(c) for c in lb_classes])
+
+ens = RegionexpertEnsemble(val_fold=list(range(5)),
+                           data_name='OV04',
+                           preprocessed_name=p_name, 
+                           model_name=model_name)
+
+while not ens.all_folds_complete():
+    sleep(60)
+
+ens.eval_raw_dataset('BARTS')
