6826b1d41096882bd9d5c2fef2a5c1e309fc6ae7
ThomasBudd <tb588@cam.ac.uk>
2021-10-20 09:43:21 +0200

    added checking of regions

--- ovseg/data/RegionexpertDataloader.py
+++ ovseg/data/RegionexpertDataloader.py
@@ -201,10 +201,14 @@ class RegionexpertBatchDataset(object):
                 if reg_coords.shape[1] > 0:
                     self.contains_reg_list.append(ind)
 
+        # print how many scans we have with which class
+        for c in range(self.n_fg_classes):
+            print('Found {} scans with fg {}'.format(len(self.contains_fg_list[c]), c))
+
         # available classes start from 0
         self.availble_classes = [i for i, l in enumerate(self.contains_fg_list) if len(l) > 0]
         
-        assert len(self.availble_classes) > 0, 'no fg classes found!'
+        # assert len(self.availble_classes) > 0, 'no fg classes found!'
         if len(self.availble_classes) < self.n_fg_classes:
             missing_classes = [i+1 for i, l in enumerate(self.contains_fg_list) if len(l) == 0]
             print('Warning! Some fg classes were not found in this dataset. '
@@ -270,12 +274,15 @@ class RegionexpertBatchDataset(object):
         if biased_sampling:
             # when we do biased sampling we have to make sure that the
             # volume we're sampling actually has fg
-            cl = np.random.choice(self.availble_classes)
-            return np.random.choice(self.contains_fg_list[cl]), cl
+            if len(self.availble_classes) > 0:
+                cl = np.random.choice(self.availble_classes)
+                return np.random.choice(self.contains_fg_list[cl]), cl
+            else:
+                return np.random.choice(self.contains_reg_list), -1
         else:
             # in theory we should only store only preprocessed volumes with regions,
             # we still keep this just to be sure
-            return np.random.choice(self.contains_reg_list), 0
+            return np.random.choice(self.contains_reg_list), -1
 
     def __len__(self):
         return self.epoch_len * self.batch_size
@@ -291,7 +298,7 @@ class RegionexpertBatchDataset(object):
         volumes = self._get_volume_tuple(ind)
         shape = np.array(volumes[0].shape)[1:]
 
-        if biased_sampling:
+        if biased_sampling and cl >= 0:
             # let's get the list of bias coordinates
             if self.store_coords_in_ram:
                 # loading from RAM
