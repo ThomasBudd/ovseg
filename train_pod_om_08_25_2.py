f940acf0860d926ef3ea0ea3fc61e0a581cad462
ThomasBudd <tb588@cam.ac.uk>
2021-11-02 11:51:02 +0100

    updated script

--- train_pod_om_08_25_2.py
+++ train_pod_om_08_25_2.py
@@ -6,7 +6,6 @@ from time import sleep
 
 parser = argparse.ArgumentParser()
 parser.add_argument("vf", type=int)
-parser.add_argument("exp", type=int)
 args = parser.parse_args()
 
 patch_size = [64, 216, 216]
@@ -18,10 +17,7 @@ out_shape = [[40, 128, 128],
              [64, 216, 216]]
 larger_res_encoder = False
 
-if args.exp == 0:
-    data_name = 'BARTS_dcm_OV04_dcm'
-else:
-    data_name = 'ApolloTCGA_dcm_BARTS_dcm_OV04_dcm'
+data_name = 'ApolloTCGA_dcm_BARTS_dcm_OV04_dcm'
 
 model_params = get_model_params_3d_res_encoder_U_Net(patch_size=patch_size,
                                                      z_to_xy_ratio=2.5/0.8,
@@ -35,6 +31,10 @@ model_params['data']['val_dl_params']['n_fg_classes'] = 2
 model_params['data']['trn_dl_params']['n_fg_classes'] = 2
 model_params['data']['val_dl_params']['bias'] = 'cl_fg'
 model_params['data']['trn_dl_params']['bias'] = 'cl_fg'
+model_params['postprocessing'] = {'apply_small_component_removing': True,
+                                  'volume_thresholds': 10,
+                                  'remove_2d_comps': True,
+                                  'use_fill_holes_2d': True}
 
 p_name = 'pod_om_08_25'
 model = SegmentationModel(val_fold=args.vf,
@@ -43,16 +43,3 @@ model = SegmentationModel(val_fold=args.vf,
                           model_name=model_name,
                           model_parameters=model_params)
 model.training.train()
-
-if args.exp == 0:
-    model.eval_raw_data_npz('ApolloTCGA_dcm')
-
-    ens = SegmentationEnsemble(val_fold=list(range(5, 10)),
-                               data_name='OV04',
-                               preprocessed_name=p_name, 
-                               model_name=model_name)
-    
-    while not ens.all_folds_complete():
-        sleep(60)
-    
-    ens.eval_raw_dataset('ApolloTCGA_dcm')
