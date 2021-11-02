447ef300951b5882b6982b12c7957100274e5f54
ThomasBudd <tb588@cam.ac.uk>
2021-11-02 18:53:36 +0100

    removed bug

--- inference_ApolloTCGA_ensemble.py
+++ inference_ApolloTCGA_ensemble.py
@@ -6,7 +6,7 @@ from ovseg.model.SegmentationEnsemble import SegmentationEnsemble
 p_name='pod_om_08_25'
 model_name='U-Net4_prg_lrn'
 path_to_params = join(environ['OV_DATA_BASE'], 'trained_models', 
-                      'OV04', p_name, model_name, 'model_paramters.pkl')
+                      'OV04', p_name, model_name, 'model_parameters.pkl')
 
 model_params = load_pkl(path_to_params)
 
@@ -20,6 +20,6 @@ save_pkl(model_params, path_to_params)
 ens = SegmentationEnsemble(val_fold=[5,6,7],
                            data_name='OV04',
                            model_name=model_name, 
-                           preprocessed_name=preprocessed_name)
+                           preprocessed_name=p_name)
 
 ens.eval_raw_dataset('ApolloTCGA_dcm')
