b8d7e84b4393f48e3fb259d4fccdacdb8b8436ed
ThomasBudd <tb588@cam.ac.uk>
2021-11-15 14:07:34 +0100

    updated script

--- predict_pod_om_TCGA_new.py
+++ predict_pod_om_TCGA_new.py
@@ -71,7 +71,7 @@ for i in tqdm(range(len(scans))):
     if not scanp.endswith('pelvis'):
         # load the liver segmentation
         nii_file = os.path.join(livp, data_tpl['pat_id']+'_liver_TB.nii.gz')
-        liver = read_nii(nii_file)
+        liver = read_nii(nii_file)[0]
         
         # compute the slice where the liver is the largest
         z_liver = np.argmax(np.sum(liver, (1,2)))
