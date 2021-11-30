4bc0a4018a1bed0d8fea5bde3b645b86f733fe7e
ThomasBudd <tb588@cam.ac.uk>
2021-11-30 11:51:25 +0100

    added script for the new experiments

--- preprocess_multiclass_reg_expert.py
+++ preprocess_multiclass_reg_expert.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("w", type=int)
 args = parser.parse_args()
 
-w = 0
+w = 0.001
 
 prep = RegionexpertPreprocessing(apply_resizing=True,
                                  apply_pooling=False,
@@ -13,9 +13,9 @@ prep = RegionexpertPreprocessing(apply_resizing=True,
                                  region_finding_model={'data_name': 'OV04',
                                                        'preprocessed_name': 'multiclass_reg',
                                                        'model_name': 'regfinding_'+str(w)},
-                                 lb_classes=[1],
+                                 lb_classes=[1, 9],
                                  target_spacing=[5.0, 0.67, 0.67],
                                  save_only_fg_scans=False)
 
 prep.plan_preprocessing_raw_data('OV04')
-prep.preprocess_raw_data('OV04', 'multiclass_reg_expert_om')
+prep.preprocess_raw_data('OV04', 'reg_expert_1_9')
