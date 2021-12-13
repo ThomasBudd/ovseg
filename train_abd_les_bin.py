cffe5620a8aff04f45964f3a94760ecaf703179d
ThomasBudd <tb588@cam.ac.uk>
2021-12-13 12:19:50 +0100

    updated binary to multiclass segmentation

--- train_abd_les_bin.py
+++ train_abd_les_bin.py
@@ -6,7 +6,7 @@ from ovseg.model.model_parameters_segmentation import get_model_params_3d_res_en
 from ovseg import OV_PREPROCESSED
 
 data_name = 'OV04'
-p_name = 'abd_les_bin'
+p_name = 'abd_1_to_7'
 model_name = 'U-Net4'
 
 ppath = os.path.join(OV_PREPROCESSED, data_name, p_name)
@@ -17,8 +17,7 @@ if not os.path.exists(ppath):
                                      apply_pooling=False,
                                      apply_windowing=True,
                                      lb_classes=[1,2,3,4,5,6,7],
-                                     save_only_fg_scans=True,
-                                     reduce_lb_to_single_class=True)
+                                     save_only_fg_scans=True)
     
     prep.plan_preprocessing_raw_data(data_name)
     
@@ -36,7 +35,7 @@ model_params = get_model_params_3d_res_encoder_U_Net(patch_size=patch_size,
                                                      z_to_xy_ratio=5.0/0.8,
                                                      use_prg_trn=use_prg_trn,
                                                      larger_res_encoder=larger_res_encoder,
-                                                     n_fg_classes=1,
+                                                     n_fg_classes=7,
                                                      out_shape=out_shape)
 for vf in [5,6,7]:
     model = SegmentationModel(val_fold=vf,
