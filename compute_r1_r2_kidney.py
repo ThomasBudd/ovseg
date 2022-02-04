1a27bae3b4f38e593e33a33b1c14a5d8e92ede40
ThomasBudd <tb588@cam.ac.uk>
2022-02-04 15:42:42 +0100

    added first script to compute r1 and r2

---
+++ compute_r1_r2_kidney.py
@@ -0,0 +1,83 @@
+import numpy as np
+from ovseg.utils.seg_fg_dial import seg_eros, seg_fg_dial
+from ovseg.data.Dataset import raw_Dataset
+from torch.nn.functional import interpolate
+import torch
+from tqdm import tqdm
+import os
+
+prev_stage = {'data_name': 'kits21',
+              'preprocessed_name': 'kidney_low',
+              'model_name': 'first_try'}
+pred_key = '_'.join(['prediction',
+                     prev_stage['data_name'],
+                     prev_stage['preprocessed_name'],
+                     prev_stage['model_name']])
+
+
+ds = raw_Dataset('kits21', prev_stages=prev_stage)
+
+target_spacing = np.array([3.0, 0.8, 0.8])
+z_to_xy_ratio = 3.0/0.8
+
+r_max = 30
+
+
+def compute_r1_r2(label, pred):
+        
+    # first r1
+    vol_lb = np.sum(label)
+    
+    r1 = 0
+    
+    ovlp = np.sum(label * pred)
+    if ovlp < vol_lb:
+        
+        while ovlp < vol_lb and r1 < r_max:
+            r1 += 1
+            pred_dial = seg_fg_dial(pred, r1, z_to_xy_ratio=z_to_xy_ratio, use_3d_ops=True)
+            ovlp = np.sum(label * pred_dial)
+    
+    r2 = 0
+    
+    ovlp = np.sum(label * pred)
+    if ovlp > 0:
+        while ovlp > 0 and r2 < r_max:
+            r2 += 1
+            pred_eros = seg_eros(pred, r2, z_to_xy_ratio=z_to_xy_ratio, use_3d_ops=True)
+            ovlp = np.sum(pred_eros * label)
+        
+    return r1, r2
+
+def get_label_pred(data_tpl):
+    # reduce to binary label
+    lb = (data_tpl['label'] > 0).astype(float)
+    pred = data_tpl[pred_key]
+    
+    # compute rescaling factor
+    spacing = data_tpl['spacing']
+    scale_factor = (spacing / target_spacing).tolist()
+    
+    # now do the interpolation
+    batch = np.stack([lb, pred])[np.newaxis]
+    batch = torch.from_numpy(batch).cuda()
+    # nearest neighbour interpolation
+    batch = interpolate(batch, scale_factor=scale_factor).cpu().numpy()
+    
+    return batch[0,0], batch[0, 1]
+
+r_list = []
+
+for i in tqdm(range(len(ds))):
+    data_tpl = ds[i]
+    label, pred = get_label_pred(data_tpl)
+    
+    r_list.append(compute_r1_r2(label, pred))
+    
+    break
+
+np.save(os.path.join(os.environ['OV_DATA_BASE'],
+                     'predicitons',
+                     'kits21',
+                     'r_list'),
+        r_list)
