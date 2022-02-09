40cbfd6c8ca1542d1019d7ded6a255b54db4e51e
ThomasBudd <tb588@cam.ac.uk>
2022-02-09 14:24:53 +0100

    added cascaded refinement

--- preprocess_kidney_contour_refinement.py
+++ preprocess_kidney_contour_refinement.py
@@ -1,11 +1,11 @@
-from ovseg.preprocessing.ContourRefinementV2Preprocessing import ContourRefinementV2Preprocessing
+from ovseg.preprocessing.ContourRefinementV3Preprocessing import ContourRefinementV3Preprocessing
 
 
 prev_stages = {'data_name': 'kits21',
                'preprocessed_name': 'kidney_low',
                'model_name': 'first_try'}
 
-preprocessing = ContourRefinementV2Preprocessing(apply_resizing=True,
+preprocessing = ContourRefinementV3Preprocessing(apply_resizing=True,
                                                apply_pooling=False,
                                                apply_windowing=True,
                                                reduce_lb_to_single_class=True,
@@ -16,4 +16,4 @@ preprocessing = ContourRefinementV2Preprocessing(apply_resizing=True,
                                                r_dial=10)
 
 preprocessing.preprocess_raw_data(raw_data='kits21',
-                                  preprocessed_name='kidney_full_refine')
+                                  preprocessed_name='kidney_full_refine_v3')
