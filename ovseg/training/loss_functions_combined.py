df952d9c78cdf5ab6c7b1e92363439d5e1349b59
ThomasBudd <tb588@cam.ac.uk>
2022-02-14 11:06:25 +0100

    removed bug

--- ovseg/training/loss_functions_combined.py
+++ ovseg/training/loss_functions_combined.py
@@ -83,7 +83,7 @@ def downsample_yb(logs_list, yb):
     f = F.adaptive_max_pool3d if len(yb.shape) == 5 else F.adaptive_max_pool2d
     
     # target downsampled to same size as logits
-    return [f(yb, logs.size[2:]) for logs in logs_list]
+    return [f(yb, logs.shape[2:]) for logs in logs_list]
     
 
 def downsample_yb_old(logs_list, yb):
