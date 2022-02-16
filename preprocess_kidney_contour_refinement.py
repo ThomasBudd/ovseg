3da312b1d916eb1e26855f8c738b3e34147d012f
ThomasBudd <tb588@cam.ac.uk>
2022-02-16 11:55:05 +0100

    added kindey segmentation scripts

--- preprocess_kidney_contour_refinement.py
+++ preprocess_kidney_contour_refinement.py
@@ -2,18 +2,18 @@ from ovseg.preprocessing.ContourRefinementV3Preprocessing import ContourRefineme
 
 
 prev_stages = {'data_name': 'kits21',
-               'preprocessed_name': 'kidney_low',
-               'model_name': 'ps_64_bs16'}
+               'preprocessed_name': 'kidney_full_refine_new',
+               'model_name': 'refine_model'}
 
 preprocessing = ContourRefinementV3Preprocessing(apply_resizing=True,
-                                               apply_pooling=False,
-                                               apply_windowing=True,
-                                               reduce_lb_to_single_class=True,
-                                               prev_stages=prev_stages,
-                                               target_spacing = [3.0, 0.78125, 0.78125],
-                                               window = [-61.5, 309.5],
-                                               scaling = [ 73.998665, 104.83186 ],
-                                               r_dial=10)
+                                                 apply_pooling=False,
+                                                 apply_windowing=True,
+                                                 reduce_lb_to_single_class=True,
+                                                 prev_stages=prev_stages,
+                                                 target_spacing = [3.0, 0.78125, 0.78125],
+                                                 window = [-61.5, 309.5],
+                                                 scaling = [ 73.998665, 104.83186 ],
+                                                 r_dial=10)
 
 preprocessing.preprocess_raw_data(raw_data='kits21',
-                                  preprocessed_name='kidney_full_refine_new')
+                                  preprocessed_name='kidney_full_refine_refine')
