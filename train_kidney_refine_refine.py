e53e26c04a5d0643d0ae2a3077c2aa491c27d4ef
ThomasBudd <tb588@cam.ac.uk>
2022-02-18 11:06:03 +0100

    updated hyper-parameters

--- train_kidney_refine_refine.py
+++ train_kidney_refine_refine.py
@@ -8,20 +8,19 @@ parser = argparse.ArgumentParser()
 parser.add_argument("vf", type=int)
 args = parser.parse_args()
 
-patch_size = [16, 64, 64]
+patch_size = [24, 96, 96]
 
 # compute shapes for progressive learning with this magic formula!
-sizes = 4*np.round(patch_size[2] / np.arange(4,0,-1)**(1/3) / 4)
-out_shape = [ [int(s)//4,  int(s), int(s)] for s in sizes]
-print(out_shape)
+# sizes = 4*np.round(patch_size[2] / np.arange(4,0,-1)**(1/3) / 4)
+# out_shape = [ [int(s)//4,  int(s), int(s)] for s in sizes]
+# print(out_shape)
 
 data_name = 'kits21'
 p_name = 'kidney_full_refine_refine'
 
 model_params = get_model_params_3d_res_encoder_U_Net(patch_size=patch_size,
                                                      z_to_xy_ratio=3.0/0.8,
-                                                     use_prg_trn=True,
-                                                     out_shape=out_shape,
+                                                     use_prg_trn=False,
                                                      larger_res_encoder=False,
                                                      n_fg_classes=1)
 
@@ -34,8 +33,8 @@ model_params['network']['norm'] = 'batch'
 model_params['data']['folders'] = ['images', 'labels', 'regions', 'prev_preds']
 model_params['data']['keys'] = ['image', 'label', 'region', 'prev_pred']
 for s in ['trn_dl_params', 'val_dl_params']:
-    model_params['data'][s]['batch_size'] = 32
-    model_params['data'][s]['min_biased_samples'] = 11
+    model_params['data'][s]['batch_size'] = 8
+    model_params['data'][s]['min_biased_samples'] = 3
     model_params['data'][s]['num_workers'] = 14
 model_params['training']['batches_have_masks'] = True
 model_params['training']['opt_params']['weight_decay'] = 2e-4
