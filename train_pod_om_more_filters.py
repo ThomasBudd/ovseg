4262c7152f3b8611b67289e85b7503334e423e0d
ThomasBudd <tb588@cam.ac.uk>
2022-03-18 12:15:14 +0100

    added new training script

--- train_pod_om_more_filters.py
+++ train_pod_om_more_filters.py
@@ -4,10 +4,6 @@ from ovseg.model.model_parameters_segmentation import get_model_params_3d_res_en
 import argparse
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument("exp", type=int)
-args = parser.parse_args()
-
 data_name = 'OV04'
 preprocessed_name = 'pod_om_08_5'
 
@@ -21,13 +17,9 @@ out_shape = [[20, 128, 128],
              [32, 216, 216]]
 larger_res_encoder = False
 
-
-momentum = [0.98, 0.98, 0.97][args.exp]
-min_bias_samples = [1, 2, 2][args.exp]
-norm = 'batch'
-batch_size = 6
-
-
+momentum = 0.98
+min_bias_samples = 1
+batch_size = 4
 
 model_params = get_model_params_3d_res_encoder_U_Net(patch_size,
                                                      z_to_xy_ratio=5.0/0.8,
@@ -35,9 +27,9 @@ model_params = get_model_params_3d_res_encoder_U_Net(patch_size,
                                                      n_fg_classes=2,
                                                      use_prg_trn=use_prg_trn)
 model_params['architecture'] = 'unetresencoderv2'
-model_params['network']['norm'] = norm
 del model_params['network']['block']
 del model_params['network']['stochdepth_rate']
+model_params['network']['filters'] = [32, 64, 160, 384]
 model_params['data']['trn_dl_params']['batch_size'] = batch_size
 model_params['data']['val_dl_params']['batch_size'] = batch_size
 model_params['data']['trn_dl_params']['min_bias_samples'] = min_bias_samples
@@ -46,7 +38,7 @@ model_params['training']['opt_params']['momentum'] = momentum
 model_params['training']['opt_params']['weight_decay'] = wd
 
 # change the model name when using other hyper-paramters
-model_name = f'bs{batch_size}_{norm}_{momentum}_{min_bias_samples}'
+model_name = f'bs{batch_size}_inst_more_filters'
 for vf in [5,6,7]:
     model = SegmentationModel(val_fold=vf,
                               data_name=data_name,
