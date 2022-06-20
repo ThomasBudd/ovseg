1e8ada84920ee9a3c18992a9c00576ab1ccd05b2
ThomasBudd <tb588@cam.ac.uk>
2022-06-20 13:13:29 +0200

    added script

---
+++ compute_scaling_coeff_UQ.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from time import sleep
+import pickle
+
+predp = os.path.join(os.environ['OV_DATA_BASE'], 'predictions','OV04','pod_om_4fCV')
+rawp = os.path.join(os.environ['OV_DATA_BASE'], 'raw_data')
+
+# %%
+n_ens = 7
+w_list = list(range(-3,4))
+
+k_vec = np.zeros(n_ens, 2)
+n_vec = np.zeros(n_ens, 2)
+
+    
+scans = [s for s in os.listdir(os.path.join(rawp, 'OV04','labels'))
+         if s.endswith('.nii.gz')]
+
+for scan in tqdm(scans):
+    
+    gt_seg = nib.load(os.path.join(rawp, 'OV04', 'labels', scan)).get_fdata()
+    for i, cl in enumerate([1, 9]):
+        gt = (gt_seg == cl).astype(float)
+        hm = np.zeros_like(gt)
+        
+        for w in w_list:
+            
+            model_name = f'claibrated_{w:.2f}'
+            
+            pred = nib.load(os.path.join(predp, model_name, 'cross_validation', scan)).get_fdata()
+            hm += (pred == cl).astype(float)
+        
+        for c in range(n_ens):
+            I = (hm == c+1).astype(float)
+            n_vec[c] += np.sum(I)
+            k_vec[c] += np.sum(I*gt)
+
+p_vec = k_vec/n_vec
+np.save(os.path.join(predp, 'P_cross_validation.npy'), p_vec)
