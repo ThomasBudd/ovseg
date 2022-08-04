a66f746c2848015a2490261ab349dd8891788542
ThomasBudd <tb588@cam.ac.uk>
2022-08-04 23:08:35 +0200

    added more scripts

--- dropout_weight_decay_check.py
+++ dropout_weight_decay_check.py
@@ -11,16 +11,16 @@ models = [f'dropout_UNet_{i}' for i in range(4)]
 
 mean_scores = np.zeros((4,4))
 
-for i, model in models:
+for i, model in enumerate(models):
     
     res_B_list = [pickle.load(open(os.path.join(bp, model, 'fold_5',
-                                                f'BARTS_{j}_results.pkl'))) for j in range(8)]
+                                                f'BARTS_{j}_results.pkl'), 'rb')) for j in range(8)]
     res_A_list = [pickle.load(open(os.path.join(bp, model, 'fold_5',
-                                                f'ApolloTCGA_{j}_results.pkl'))) for j in range(8)]
+                                                f'ApolloTCGA_{j}_results.pkl'), 'rb')) for j in range(8)]
     
-    mean_scores[i, 0] = np.mean([np.nanmean([res_B_list[case]['dice_1'] for case in res_B_list])])
-    mean_scores[i, 2] = np.mean([np.nanmean([res_B_list[case]['dice_9'] for case in res_B_list])])
-    mean_scores[i, 1] = np.mean([np.nanmean([res_A_list[case]['dice_1'] for case in res_B_list])])
-    mean_scores[i, 3] = np.mean([np.nanmean([res_B_list[case]['dice_9'] for case in res_B_list])])
+    mean_scores[i, 0] = np.mean([np.nanmean([res[case]['dice_1'] for case in res]) for res in res_B_list])
+    mean_scores[i, 2] = np.mean([np.nanmean([res[case]['dice_9'] for case in res]) for res in res_B_list])
+    mean_scores[i, 1] = np.mean([np.nanmean([res[case]['dice_1'] for case in res]) for res in res_A_list])
+    mean_scores[i, 3] = np.mean([np.nanmean([res[case]['dice_9'] for case in res]) for res in res_A_list])
 
 print(mean_scores)
