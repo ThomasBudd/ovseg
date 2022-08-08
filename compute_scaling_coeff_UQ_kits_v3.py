5db39687aca4b1046b0c905889fd2c5b7303c87c
ThomasBudd <tb588@cam.ac.uk>
2022-08-08 14:28:48 +0200

    added script

---
+++ compute_scaling_coeff_UQ_kits_v3.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import nibabel as nib
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from time import sleep
+import pickle
+
+predp = os.path.join(os.environ['OV_DATA_BASE'], 'predictions','kits21_trn','disease_3_1')
+rawp = os.path.join(os.environ['OV_DATA_BASE'], 'raw_data')
+
+# %%
+n_ens = 7
+w_list = list(range(-3,4))
+
+k_vec = np.zeros((2,128))
+n_vec = np.zeros((2,128))
+
+    
+scans = [s for s in os.listdir(os.path.join(rawp, 'kits21_trn','labels'))
+         if s.endswith('.nii.gz')]
+
+for scan in tqdm(scans):
+    
+    gt_seg = nib.load(os.path.join(rawp, 'kits21_trn', 'labels', scan)).get_fdata()
+    for c, cl in enumerate([2]):
+        gt = (gt_seg == cl).astype(float)
+        preds = np.zeros_like(gt)
+        
+        for i, w in enumerate(w_list):
+            
+            model_name = f'UQ_calibrated_{w:.2f}'
+            
+            pred = nib.load(os.path.join(predp, model_name, 'cross_validation', scan)).get_fdata()
+            pred += 2**i * (pred == cl).astype(float)
+        
+        for i in range(2**7):
+            
+            I = preds == (i+1)
+            
+            n_vec[c,i] += np.sum(I.astype(int))
+            k_vec[c,i] += np.sum(gt[I])
+
+
+
+np.save(os.path.join(predp, 'k_cross_validation_v3.npy'), k_vec)
+np.save(os.path.join(predp, 'n_cross_validation_v3.npy'), n_vec)
+print(k_vec/n_vec)
